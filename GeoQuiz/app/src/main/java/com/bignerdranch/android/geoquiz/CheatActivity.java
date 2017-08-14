ea2f1cdac59fa7a5ee77e5e3af291794c77ab194
Antony <antony_huang@sae.com.hk>
2017-08-14 09:50:09 +0800

    修改新activity的UI

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -8,6 +8,7 @@ public class CheatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // setContentView()将activity和界面(layout)联系起来
         setContentView(R.layout.activity_cheat);
     }
 }
