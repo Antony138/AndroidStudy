5258aadf167ad897ef2ce7ba5544fd7774da23e6
Antony <antony_huang@sae.com.hk>
2017-08-15 15:56:59 +0800

    获取FragmentManager对象；fragment transaction

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +12,21 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        // 获取FragmentManager对象
+        FragmentManager fm = getSupportFragmentManager();
+
+        // Fragment transactions
+        // 通过ID拿到CrimeFragment，如果有在fragament list中，就会返回。
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        // 如果fragment为null(不在fragment list中)，需要创建。
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            // 以下这句是创建和commits一个fragment transaction
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
