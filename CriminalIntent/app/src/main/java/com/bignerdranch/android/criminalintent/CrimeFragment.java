f8eeab6d2cecddfe757b5fa164b6a380077a17a8
Antony <antony_huang@sae.com.hk>
2017-08-15 11:18:13 +0800

    create a new Fragment subclass

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by sae_antony on 15/08/2017.
+ */
+
+// extends Fragment是表示：CrimeFragment是Fragment的子类吗？
+// 有两个Fragment的，要选择(android.support.v4.app)这个
+public class CrimeFragment extends Fragment {
+
+
+}
