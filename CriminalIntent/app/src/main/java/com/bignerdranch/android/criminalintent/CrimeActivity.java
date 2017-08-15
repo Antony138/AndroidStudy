ea06bd47503ec6598e32664d7fb28eca08db6ae2
Antony <antony_huang@sae.com.hk>
2017-08-15 17:29:38 +0800

    CrimeActivity改为继承abstract class SingleFragmentActivity。

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -6,27 +6,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 // AppCompatActivity要改为FragmentActivity吗？
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-        // 获取FragmentManager对象
-        FragmentManager fm = getSupportFragmentManager();
-
-        // Fragment transactions
-        // 通过ID拿到CrimeFragment，如果有在fragament list中，就会返回。
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        // 如果fragment为null(不在fragment list中)，需要创建。
-        if (fragment == null) {
-            fragment = new CrimeFragment();
-            // 以下这句是创建和commits一个fragment transaction
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
+
+    //    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_fragment);
+//
+//        // 获取FragmentManager对象
+//        FragmentManager fm = getSupportFragmentManager();
+//
+//        // Fragment transactions
+//        // 通过ID拿到CrimeFragment，如果有在fragament list中，就会返回。
+//        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+//
+//        // 如果fragment为null(不在fragment list中)，需要创建。
+//        if (fragment == null) {
+//            fragment = new CrimeFragment();
+//            // 以下这句是创建和commits一个fragment transaction
+//            fm.beginTransaction()
+//                    .add(R.id.fragment_container, fragment)
+//                    .commit();
+//        }
+//    }
 }
