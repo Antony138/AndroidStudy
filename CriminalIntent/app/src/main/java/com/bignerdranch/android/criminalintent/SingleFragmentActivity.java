1d38df94b3ecb80b0cee9b540b38c3e2fd1c72be
Antony <antony_huang@sae.com.hk>
2017-09-01 14:18:42 +0800

    Making SingleFragmentActivity flexible.

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -14,10 +15,16 @@ import android.support.v7.app.AppCompatActivity;
 public abstract class SingleFragmentActivity extends AppCompatActivity {
     protected abstract Fragment createFragment();
 
+    // 为了增加tablet的Master-Detail的支持，需要增加这个
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
