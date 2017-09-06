58fa95830f72b59e59e9e96992f2e662d4c678cb
Antony <antony_huang@sae.com.hk>
2017-09-06 15:42:06 +0800

    import SingleFragmentActivity；Enabling data binding.

---
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/SingleFragmentActivity.java
@@ -0,0 +1,39 @@
+package com.bignerdranch.android.beatbox;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sae_antony on 15/08/2017.
+ */
+
+// 这个抽象类，是为了减少重复代码而存在的，CrimeActivity和CrimeListActivity都继承自这个类
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    // 为了增加tablet的Master-Detail的支持，需要增加这个
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
