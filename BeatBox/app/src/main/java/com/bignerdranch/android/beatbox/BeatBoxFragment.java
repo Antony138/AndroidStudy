a080e6bf2dc62feb58c01c3d9dafab7123ff5dfb
Antony <antony_huang@sae.com.hk>
2017-09-06 18:48:17 +0800

    配置RecyclerView

--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -5,11 +5,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.bignerdranch.android.beatbox.databinding.FragmentBeatBoxBinding;
+import com.bignerdranch.android.beatbox.databinding.ListItemSoundBinding;
 
 /**
  * Created by sae_antony on 06/09/2017.
@@ -28,5 +29,14 @@ public class BeatBoxFragment extends Fragment {
         binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 //        binding.recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
         return binding.getRoot();
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder {
+        private ListItemSoundBinding mBinding;
+
+        private SoundHolder(ListItemSoundBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+        }
     }
 }
