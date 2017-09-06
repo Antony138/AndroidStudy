5a8f7066a22ba438ca61f883d8f8c2f196edc17e
Antony <antony_huang@sae.com.hk>
2017-09-06 15:21:12 +0800

    修改activity_beat_box.xml为fragment_beat_box.xml；创建BeatBoxFragment。

---
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.beatbox;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by sae_antony on 06/09/2017.
+ */
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
